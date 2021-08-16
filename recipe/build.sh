ca1c09e07c24f28d3f9e2316541aa2b9254602c4
Mark Harfouche <mark.harfouche@gmail.com>
2021-08-15 20:58:03 -0400

    Add definition

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,6 +7,10 @@ set -ex
     --with-zlib=${PREFIX} \
     --with-java=${JAVA_HOME}
 
+# p2c and c2p functions are all deprecated
+# but this code makes use of them.
+# you can enable them on OSX if you define this variable
+export CPPFLAGS="${CPPFLAGS} -DOLDP2C"
 make -j${CPU_COUNT}
 
 make install
